d4ee1928e5ecd8f1706abc3ec31321075a7f0cdd
Adilson-R-Martins <adilson.martins@protonmail.com>
2024-05-19 21:50:05 -0300

    cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,6 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.google.code.gson:gson:2.11.0")
-//    implementation (com.squareup.retrofit2:retrofit:2.9.0')
-//    implementation (com.squareup.retrofit2:converter-gson:2.9.0)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
